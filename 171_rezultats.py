261c32d60102bd2fb913db307c98252443404065
user <ricardseglitis@gmail.com>
2018-12-10 11:50:15 +0200

    10_12_18_11_50

---
+++ 171_rezultats.py
@@ -0,0 +1,10 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: /home/user/RTR105/1711.py =====================
+Funkcijas atan(x) dotaja intervala sakne(s) ir!
+atan(x) sakne ir: 9.1552734375e-05
+f(x)= 9.155273411920462e-05
+k= 16
+>>> 
